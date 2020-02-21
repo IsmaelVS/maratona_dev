902f1adde0c1d720d3f8b79afcc00e7cb6187ceb
Ismael Ventura <ismael.venturadasilva0@gmail.com>
2020-02-20 22:33:54 -0300

    Criando funcionalidade para o formulário de doadores.

--- app.py
+++ app.py
@@ -1,11 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 
-@app.route('/')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+db = SQLAlchemy(app)
+
+class Doner(db.Model):
+    """Classe para criação da tabela de doadores."""
+
+    __tablename__ = 'doner'
+
+    _id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(30), nullable=False)
+    blood = db.Column(db.String(4), nullable=False)
+
+    def __init__(self, name, email, blood):
+        db.create_all()
+        self.name = name
+        self.email = email
+        self.blood = blood
+
+    def __repr__(self):
+        return f'Doner(name={name}, email={email}, blood={blood})'
+
+
+@app.route('/', methods=['POST', 'GET'])
 def home():
-    """Rota para página inicial."""
+    """Rota para exibir página e cadastrar novo doador."""
+
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        blood = request.form.get('blood')
+
+        doner = Doner(name=name, email=email, blood=blood)
+
+        db.session.add(doner)
+        db.session.commit()
+
+        return render_template('index.html')
     return render_template('index.html')
 
 
